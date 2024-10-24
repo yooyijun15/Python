c56e4bed6048cc8003c0a701ae8cf1f672bece31
yooyijun15 <uyijune15@gmail.com>
2024-10-24 16:01:59 +0900

    241024

--- Day2/project_2.py
+++ Day2/project_2.py
@@ -1,10 +1,16 @@
 print("Welcome to the TIP calcalator.")
+
+# 가격, 팁, 인원수 입력 받기
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
+
+# 팁 계산
 tip_amount = bill * (tip / 100)
+
+# 전체 및 인당 지불 금액 계산
 total_bill = bill + tip_amount
 bill_per_person = total_bill / people
-# f 문자열
+
+# 출력
 print(f"Each person should pay: ${bill_per_person:.2f}")
-# {bill_per_person:.2f} : 소숫점 2번째 자리까지 표시
