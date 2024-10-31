b8181b1cf37d3f0b5d3a10465771b860e625edd7
yooyijun15 <uyijune15@gmail.com>
2024-10-31 17:24:48 +0900

    241031

--- Day5/project.py
+++ Day5/project.py
@@ -1,5 +1,9 @@
+# 랜덤 비밀번호 생성기
+
+# random 모듈
 import random
 
+# 알파벳, 기호, 숫자 리스트
 letters = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
@@ -12,31 +16,30 @@ symbols = [
 ]
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+# 알파벳, 기호, 숫자 갯수 입력
+letter_cnt = int(input("How many letters would you like in your password? "))
+symbol_cnt = int(input("How many symbols would you like in your password? "))
+number_cnt = int(input("How many number would you like in your password? "))
+
 password_list = []
 password_char = ""
 
-
-print("Welcome to the PyPassword Generator!")
-count_password = int(
-    input("How many letters would you like in your password?\n"))
-count_symbols = int(input("How mamy symbols would you like?\n"))
-count_numbers = int(input("How many numbers would you like?\n"))
-
-
-for char in range(1, count_password + 1):
+# 비밀번호 리스트의 항목 랜덤 추가
+for i in range(0, letter_cnt):
     password_list += random.choice(letters)
 
-for char in range(1, count_symbols + 1):
+for j in range(0, symbol_cnt):
     password_list += random.choice(symbols)
 
-for char in range(1, count_numbers + 1):
+for k in range(0, number_cnt):
     password_list += random.choice(numbers)
 
-# 리스트 섞기
 random.shuffle(password_list)
+# random 함수 .shuffle()
 
-# 리스트 -> 문자열 변경
-for char in password_list:
-    password_char += char
+# 리스트 -> 문자열 변환
+for element in password_list:
+    password_char += element
 
-print(f"Here is your password: {password_char}")
+# 출력
+print(f"random password : {password_char}")
